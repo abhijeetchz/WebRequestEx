f4e7a23abb0d1aa86b9c5f44536376c1f044d088
Abhijeet Chaudhary <abhijeetchz@Abhijeets-MacBook-Pro.local>
2016-03-02 23:05:00 +0100

    added files for Pika

--- WebRequestEx/ViewController.swift
+++ WebRequestEx/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var VCContainer: UIView!
+    
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        webView = WKWebView()
+        VCContainer.addSubview(webView)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        let frame = CGRectMake(0, 0, VCContainer.bounds.width, VCContainer.bounds.height)
+        webView.frame = frame
+        
+        loadRequest("https://developer.apple.com/swift/blog/")
+        
+    }
 
+    @IBAction func btnLoadSwiftACTION(sender: UIButton) {
+        loadRequest("https://developer.apple.com/swift/blog/")
+    }
+    
+    @IBAction func btnLoadGossipACTION(sender: AnyObject) {
+        loadRequest("http://www.tmz.com")
+    }
+    
+    @IBAction func btnLoadInstagramACTION(sender: UIButton) {
+        loadRequest("http://blog.instagram.com")
+    }
+    
+    
+    func loadRequest(urlStr: String) {
+        let url = NSURL(string: urlStr)!
+        let request = NSURLRequest(URL: url)
+        webView.loadRequest(request)
+    }
 }
 
